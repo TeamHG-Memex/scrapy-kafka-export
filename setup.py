3b7b7011375b21e3300d12b1399f39de352338e1
Mikhail Korobov <kmike84@gmail.com>
2017-08-25 02:35:05 +0500

    cleanup setup.py:
    * remove scrapy from install_requires to prevent unintended upgrades;
    * add description and repo URL;
    * add Scrapy trove classifier

--- setup.py
+++ setup.py
@@ -6,22 +6,24 @@ setup(
     version='0.1.0',
     packages=['scrapy_kafka_export'],
     install_requires=[
-        'scrapy',
         'kafka-python',
         'retrying'
     ],
     license='MIT license',
+    description="Export Scrapy items to Kafka",
+    url='https://github.com/TeamHG-Memex/scrapy-kafka-export',
     classifiers=[
         'Development Status :: 3 - Alpha',
         'License :: OSI Approved :: MIT License',
         'Intended Audience :: Developers',
         'Operating System :: OS Independent',
+        'Framework :: Scrapy',
         'Programming Language :: Python',
         'Programming Language :: Python :: 2',
         'Programming Language :: Python :: 2.7',
         'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.3',
         'Programming Language :: Python :: 3.4',
         'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
     ],
 )
