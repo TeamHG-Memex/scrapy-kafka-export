f5c04ba5e27eee7f965cee41618d9bfdb6ffa97e
Mikhail Korobov <kmike84@gmail.com>
2017-08-24 19:22:28 +0500

    simplify configuration
    * settings are validated earlier, when extension is created;
    * support for spider attributes is removed, as there is custom_settings spider attribute;
    * KAFKA_ prefixes are added to all settings

--- scrapy_kafka_export/extensions.py
+++ scrapy_kafka_export/extensions.py
@@ -1,30 +1,25 @@
 # -*- coding: utf-8 -*-
 """
-Exports items to a Kafka topic
+An extension to export items to a Kafka topic.
 
 Settings
 --------
-* ``KAFKA_EXPORT_ENABLED`` - Flag that enables the extension
+* ``KAFKA_EXPORT_ENABLED`` - Flag that enables the extension;
+   it is False by default.
 * ``KAFKA_BROKERS`` - List of Kafka brokers in format host:port
 * ``KAFKA_TOPIC`` - Kafka topic where items are going to be sent
+* ``KAFKA_BATCH_SIZE`` - Kafka batch size (100 by default).
 
 If SSL connection is enabled, the certificates must be included as a python 
 module and as package data in setup.py
 
-* ``SSL_CONFIG_MODULE`` - name of the project module
-* ``SSL_CACERT_FILE`` - resource path of the Certificate Authority certificate
-* ``SSL_CLIENTCERT_FILE`` - resource path of the client certificate
-* ``SSL_CLIENTKEY_FILE`` - resource path of the client key
+* ``KAFKA_SSL_CONFIG_MODULE`` - name of the project module
+* ``KAFKA_SSL_CACERT_FILE`` - resource path of the Certificate Authority
+    certificate
+* ``KAFKA_SSL_CLIENTCERT_FILE`` - resource path of the client certificate
+* ``KAFKA_SSL_CLIENTKEY_FILE`` - resource path of the client key
 
-If ``SSL_CONFIG_MODULE`` is not set, no certificate will be loaded
-
-Spider attributes
------------------
-
-The following spider attributes are available and overrides equivalent settings:
-
-* ``kafka_export_enabled`` - Same as ``KAFKA_EXPORT_ENABLED``
-* ``kafka_topic`` - Same as ``KAFKA_TOPIC``
+If ``KAFKA_SSL_CONFIG_MODULE`` is not set, no certificate will be loaded.
 
 Usage
 -----
@@ -32,7 +27,7 @@ In ``settings.py``
 ::
 
     EXTENSIONS = {
-        'scrapy-kafka-export.extensions.KafkaItemExporterExtension': 1,
+        'scrapy-kafka-export.KafkaItemExporterExtension': 1,
     }
     
     KAFKA_EXPORT_ENABLED = True
@@ -43,10 +38,10 @@ In ``settings.py``
     ]
     KAFKA_TOPIC = 'test-topic'
         
-    SSL_CONFIG_MODULE = 'myproject'
-    SSL_CACERT_FILE = 'certificates/ca-cert.pem'
-    SSL_CLIENTCERT_FILE = 'certificates/client-cert.pem'
-    SSL_CLIENTKEY_FILE = 'certificates/client-key.pem'
+    KAFKA_SSL_CONFIG_MODULE = 'myproject'
+    KAFKA_SSL_CACERT_FILE = 'certificates/ca-cert.pem'
+    KAFKA_SSL_CLIENTCERT_FILE = 'certificates/client-cert.pem'
+    KAFKA_SSL_CLIENTKEY_FILE = 'certificates/client-key.pem'
 
 Assuming the following structure for the certificates from the 
 project 'myproject'::
@@ -74,12 +69,13 @@ the following package data should be added to ``setup.py``::
     
 """
 import logging
+from pkg_resources import resource_filename
 
 from scrapy import signals
 from scrapy.exceptions import NotConfigured
 
-from .config import KafkaItemExporterConfigs
-from .writer import UnknownTopicError, ScrapyKafkaTopicWriter
+from .writer import ScrapyKafkaTopicWriter
+from .utils import get_ssl_config
 
 logger = logging.getLogger(__name__)
 
@@ -87,42 +83,49 @@ logger = logging.getLogger(__name__)
 class KafkaItemExporterExtension(object):
     """ Kafka extension for writing items to a kafka topic """
     def __init__(self, crawler):
-        self.config = KafkaItemExporterConfigs(crawler.settings)
-        self.item_writer = None
+        settings = crawler.settings
+        if not settings.getbool('KAFKA_EXPORT_ENABLED', False):
+            raise NotConfigured
+        logger.debug('Kafka export extension is enabled')
 
+        self.kafka_brokers = settings.getlist('KAFKA_BROKERS')
+        self.kafka_topic = settings.get('KAFKA_TOPIC')
+        self.batch_size = settings.getint('KAFKA_BATCH_SIZE', 100)
+        ssl_module_name = settings.get('KAFKA_SSL_CONFIG_MODULE')
+        if ssl_module_name:
+            def _load(key):
+                return resource_filename(ssl_module_name, settings.get(key))
+            self.ssl_config = get_ssl_config(
+                cafile=_load('KAFKA_SSL_CACERT_FILE'),
+                certfile=_load('KAFKA_SSL_CLIENTCERT_FILE'),
+                keyfile=_load('KAFKA_SSL_CLIENTKEY_FILE'),
+            )
+        else:
+            self.ssl_config = {}
+
+        self.item_writer = None
         crawler.signals.connect(self.spider_opened, signals.spider_opened)
         crawler.signals.connect(self.spider_closed, signals.spider_closed)
-        crawler.signals.connect(self.process_item_scraped, signals.item_scraped)
+        crawler.signals.connect(self.process_item_scraped,
+                                signals.item_scraped)
 
     @classmethod
     def from_crawler(cls, crawler):
         return cls(crawler)
 
     def spider_opened(self, spider):
-        self.config.set_spider(spider)
-        if not self.config.is_enabled:
-            logger.debug('Kafka item exporter not enabled.')
-            return
-
-        self.initialize_kafka_producer(spider)
-
-    def initialize_kafka_producer(self, spider):
-        try:
-            self.item_writer = ScrapyKafkaTopicWriter(
-                bootstrap_servers=self.config.kafka_brokers,
-                topic=self.config.kafka_topic,
-                batch_size=self.config.batch_size,
-                **self.config.ssl_config
-            )
-        except UnknownTopicError as e:
-            logger.error(e.args[0])
-            raise NotConfigured
-        logger.debug("Kafka writer initialized.")
+        self.item_writer = ScrapyKafkaTopicWriter(
+            bootstrap_servers=self.kafka_brokers,
+            topic=self.kafka_topic,
+            batch_size=self.batch_size,
+            **self.ssl_config
+        )
+        logger.info("Kafka item writer initialized, writing to topic %s." %
+                    self.item_writer.topic)
 
     def spider_closed(self, spider):
         if self.item_writer is not None:
             self.item_writer.close()
 
     def process_item_scraped(self, item, response, spider):
-        if self.config.is_enabled:
-            self.item_writer.write_item(item)
+        self.item_writer.write_item(item)
