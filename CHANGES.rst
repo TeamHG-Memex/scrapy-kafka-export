4008ad39781039bdb6dfd4149112e37c07b84549
Mikhail Korobov <kmike84@gmail.com>
2017-08-28 14:46:19 +0500

    bump version to 0.1.1

--- CHANGES.rst
+++ CHANGES.rst
@@ -3,6 +3,11 @@ Changes
 
 .. contents::
 
+0.1.1 (2017-08-28)
+------------------
+
+- improved error messages in case of invalid Kafka topics.
+
 0.1.0 (2017-08-25)
 ------------------
 
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup
 
 setup(
     name='scrapy-kafka-export',
-    version='0.1.0',
+    version='0.1.1',
     packages=['scrapy_kafka_export'],
     install_requires=[
         'kafka-python',
